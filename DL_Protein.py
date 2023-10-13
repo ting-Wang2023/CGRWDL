2aaed59c83ad2b27b895d5bc02e13808fec5d54f
ting-Wang2023 <147496552+ting-Wang2023@users.noreply.github.com>
2023-10-13 15:21:40 +0800

    Add files via upload

---
+++ DL_Protein.py
@@ -0,0 +1,41 @@
+from Bio import SeqIO
+from numpy import mat
+import numpy as np
+import copy
+from functools import reduce
+from collections import Counter
+
+def get_all_kmer(string,k):
+    length = len(string)
+    return[string[i:i+k]for i in range(length - k+1)]
+
+
+
+def DlTree(s,pks,k,r,A,z):
+    
+
+    for i in range(0, 20 ** k):
+        pks[r[i]] = 0
+    ck = Counter(get_all_kmer(s, k))
+    k_1 = Counter(get_all_kmer(s, 1))  # k=1
+    k_2 = Counter(get_all_kmer(s, k - 1))  # k=k-1
+    # Counter(get_all_kmer(s,k-1))
+
+    for key in ck.keys():
+        p = ck[key] / (len(s) - k + 1)
+        q_1 = k_1[key[0]] / len(s)
+        q_2 = k_2[key[1:k]] / (len(s) - k + 2)
+        q_3 = k_2[key[0:k - 1]] / (len(s) - k + 2)
+        q_4 = k_1[key[k - 1]] / len(s)
+        # ck=Counter(get_all_kmer(s,k)) 
+        f = (p - ((q_1 * q_2 + q_3 * q_4) / 2)) / ((q_1 * q_2 + q_3 * q_4) / 2)
+        pks[key] = f
+
+    pks = dict(sorted(pks.items(), key=lambda kv: (kv[0])))
+    # print(pks)
+
+    l = -1
+    for value in pks.values():
+        l = l + 1
+        A[z, l] = value
+    return A
